2a093471ca8e12da1c35f11de96594a68079e7d5
jeonjehun <jeonjehun@211.105.78.42>
2017-02-17 23:27:47 +0900

    Springframeowrk.ui.Model을 이용하여 몇가지 타입의 오브젝트를 뷰에 전달하고, 뷰는 이를 출력하도록 구현.

--- board/src/main/java/com/ieee1986/board/HomeController.java
+++ board/src/main/java/com/ieee1986/board/HomeController.java
@@ -1,7 +1,9 @@
 package com.ieee1986.board;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -16,24 +18,48 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model, User user) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
+		// 문자열
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		model.addAttribute("serverTime", formattedDate);
+
+		// 기본자료형
+		int count = 5;
+		model.addAttribute("count", count);
+
+		// 리스트
+		ArrayList<String> listData = new ArrayList<String>();
+		listData.add("a");
+		listData.add("b");
+		listData.add("c");
+		model.addAttribute("listData", listData);
+
+		// 맵
+		HashMap<String, String> mapData = new HashMap<String, String>();
+		mapData.put("key1", "value1");
+		mapData.put("key2", "value2");
+		mapData.put("key3", "value3");
+		model.addAttribute("mapData", mapData);
+
+		// 오브젝트
+		user = new User();
+		user.setId("userId");
+		user.setName("userName");
+		model.addAttribute("userData", user);
+
 		return "home";
 	}
-	
 }
+
--- board/src/main/webapp/WEB-INF/views/home.jsp
+++ board/src/main/webapp/WEB-INF/views/home.jsp
@@ -10,5 +10,17 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+
+<P>  List Data ${listData.get(0)}. </P>
+<P>  List Data ${listData.get(1)}. </P>
+<P>  List Data ${listData.get(2)}. </P>
+
+<P>  Map Data ${mapData.key1}. </P>
+<P>  Map Data ${mapData.key2}. </P>
+<P>  Map Data ${mapData.key3}. </P>
+
+<P>  User Data ${userData.id}. </P>
+<P>  User Data ${userData.name}. </P>
+
 </body>
 </html>
